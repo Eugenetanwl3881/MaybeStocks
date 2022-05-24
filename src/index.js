aebc9195011bf295ae145b508cd65cf816917627
Effixion <syt1702@gmail.com>
2022-05-24 15:35:29 +0800

    Added Firebase Config. Added useAuth hook. Wrapped the App with a ProvideAuth provider

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import CssBaseline from '@mui/material/CssBaseline';
+import { ProvideAuth } from './hooks/useAuth,js';
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <>
     <CssBaseline />
-    <App />
+    <ProvideAuth>
+        <App />
+    </ProvideAuth>
     </>
 );
 
