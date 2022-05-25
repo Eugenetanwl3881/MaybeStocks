e13ced8ee379895e39775ceb0212bd33b115d8a3
Effixion <syt1702@gmail.com>
2022-05-25 16:05:09 +0800

    Added a welcome message
    Displays the google account name that the user used to log in with

--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -1,9 +1,21 @@
 import { useState, useEffect } from "react";
+import { useAuth } from "../hooks/useAuth";
 
 function MainPage() {
+
+    const { user } = useAuth();
+
+    const [name, setName] = useState("Loading name...");
+
+    useEffect(() => {
+        if (user?.displayName) {
+            setName(user.displayName);
+        }
+    }, [user]);
+
     return(
     <>
-        <h1>Main</h1>
+        <h1>Welcome back, {name}!</h1>
     </>
     )
 }
