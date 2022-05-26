3d928751c123cd6f75fbea5d82c554a330f0f4db
Effixion <syt1702@gmail.com>
2022-05-26 15:20:47 +0800

    added firebase and skeletal code for portfolio page

---
+++ src/Pages/PortfolioPage.js
@@ -0,0 +1,34 @@
+// import { useState, useEffect } from "react";
+// import { useAuth, db } from "../hooks/useAuth";
+// import { doc, setDoc, getDoc } from "firebase/firestore"; 
+
+
+function PortfolioPage() {
+
+    // const [transactions, setTransactions] = useState([]);
+
+    // const { user } = useAuth();
+
+    // function addTransactions(newTransactions) {
+    //     setTransactions(newTransactions);
+    //     setDoc(doc(db, "Transactions", user?.uid), { transactions: newTransactions});
+    // }
+
+    // useEffect(() => {
+    //     async function fetchData() {
+    //         const docSnapshot = await getDoc(doc(db, "transactions", user?.uid));
+    //         if (docSnapshot.exists()) {
+    //             setTransactions(docSnapshot.data().transactions);
+    //         } else {
+    //             setTransactions([]);
+    //         }
+    //     }
+    // }, [user.uid]);
+
+    // return (
+    //     <>
+    //         <h1>Transactions</h1>
+
+    //     </>
+    // )
+}
