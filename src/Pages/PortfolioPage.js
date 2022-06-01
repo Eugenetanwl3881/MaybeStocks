01f88a327f8bc1c15a92303859b365766b6f0ff1
Effixion <syt1702@gmail.com>
2022-06-01 16:04:52 +0800

    Added skeletal database to Portfolio Page. Added Portfolio Table

--- src/Pages/PortfolioPage.js
+++ src/Pages/PortfolioPage.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
+import { useAuth, db } from "../hooks/useAuth";
+import { doc, setDoc, getDoc } from "firebase/firestore";
+import PortfolioTable from "../components/PortfolioTable/PortfolioTable"
 
 function PortfolioPage() {
+  const [name, setName] = useState("Loading name...");
+
+  const [portfolio, setPortfolioState] = useState([]);
+
+  const { user } = useAuth();
+
+  useEffect(() => {
+    if (user?.displayName) {
+      setName(user.displayName);
+    }
+  }, [user]);
+
+  function setPortfolio(newPortfolios) {
+    setPortfolioState(newPortfolios);
+    setDoc(doc(db, "Portfolios", user?.uid), { [portfolio]: newPortfolios });
+  }
+
+  useEffect(() => {
+    async function fetchData() {
+      const docSnapshot = await getDoc(doc(db, "Portfolios", user?.uid));
+      if (docSnapshot.exists()) {
+        setPortfolioState(docSnapshot.data().portfolio);
+      } else {
+        setPortfolioState([]);
+      }
+    }
+    fetchData();
+  }, [user.uid]);
+
   return (
-    <div>PortfolioPage</div>
-  )
+    <>
+      <h1>Portfolio Page</h1>
+      <h1>Portfolio of {name}.</h1>
+
+      <div>
+        <PortfolioTable
+          portfolio={portfolio}
+          setPortfolio={setPortfolio}
+        />
+      </div>
+    </>
+  );
 }
 
-export default PortfolioPage
+export default PortfolioPage;
