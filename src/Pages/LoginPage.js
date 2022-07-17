e2b1fc8aa0a3a93fbb294dc44af71706ab3ccc2e
Eugenetanwl3881 <Eugene.tan.wl@gmail.com>
2022-07-17 22:10:45 +0800

    Added Logo on Login Page
    Added Logo on Login Page

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -1,14 +1,14 @@
 import { useAuth } from "../hooks/useAuth";
 import Button from "@mui/material/Button";
 import SignIn from "../components/SignIn/SignIn";
+import logo from "../images/maybestockslogo.png";
 
 function LoginPage() {
   const { signInWithGoogle } = useAuth();
 
   return (
     <>
-      <h2>Login</h2>
-      <img src="../images/Maybe_Stocks_(3).png" />
+      <img src={logo} alt="img" width="420px" height="auto" />
       <SignIn />
       <Button variant="contained" color="primary" onClick={signInWithGoogle}>
         Sign in with Google
