e24fd04327c2adce8c38d023fc91e5e8976077d1
Effixion <syt1702@gmail.com>
2022-07-19 16:45:50 +0800

    Standardised the looks of all our buttons

--- src/components/Popup/Popup.js
+++ src/components/Popup/Popup.js
@@ -1,14 +1,21 @@
 import React from "react";
 import "./Popup.css";
+import Button from "@mui/material/Button";
 
 function Popup(props) {
   return props.trigger ? (
     <div className="popup">
       <div className="popup-inner">
-        <button className="close-btn" onClick={() => props.setTrigger(false)}>
-          close
-        </button>
         {props.children}
+        <Button
+          variant="contained"
+          onClick={() => props.setTrigger(false)}
+          padding="20px"
+          size="small"
+          sx={{ p: 1, m: 1 }}
+        >
+          Close
+        </Button>
       </div>
     </div>
   ) : (
