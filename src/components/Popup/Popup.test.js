4a5b3c752c378f506e14db64cca0f19bd6c0f61e
Eugenetanwl3881 <Eugene.tan.wl@gmail.com>
2022-07-20 15:14:39 +0800

    Updated Input field for forum and added new test
    Updated Input field for forum and added new test for Pop Up

--- src/components/Popup/Popup.test.js
+++ src/components/Popup/Popup.test.js
@@ -8,3 +8,11 @@ describe("Popup should show when trigger is true", () => {
     expect(popup).toBeTruthy();
   });
 });
+
+describe("Popup should not show when trigger is true", () => {
+  it("Popup does not shows", () => {
+    const { queryByTestId } = render(<Popup trigger={false} />);
+    const popup = queryByTestId("popup");
+    expect(popup).toBeFalsy();
+  });
+});
